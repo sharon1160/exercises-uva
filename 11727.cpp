37f5476984521f6767a6f7dc57e0233a605c9f1e
Sharon CR <schullunquia@unsa.edu.pe>
2021-06-19 21:28:09 -0500

    Add solution to exercise 11727.

---
+++ 11727.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int a,b,c,t,count=0;
+  vector<int> sueldos;
+  cin>>t;
+  while(t--){
+    cin>>a>>b>>c;
+    sueldos.push_back(a);
+    sueldos.push_back(b);
+    sueldos.push_back(c);
+    sort(sueldos.begin(),sueldos.end());
+    cout<<"Case "<<count+1<<": "<<sueldos[1]<<endl;
+    sueldos.erase(sueldos.begin(),sueldos.end());
+    count++;
+  }
+}
